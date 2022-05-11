32294c94327047601c853fd84d5a9250f9aff029
nhuhoa <htran@bccrc.ca>
2022-05-11 22:54:37 +0000

    add cnv profiles for each series

--- materials/compress_csv.sh
+++ materials/compress_csv.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-search_dir=/home/htran/Projects/farhia_project/drug_resistant_material/materials/biodatabase/
+search_dir=/home/htran/Projects/farhia_project/drug_resistant_material/materials/cell_clones/
 
 input_csvs=$(find $search_dir -type f -name "*.csv")
 
