fd6961d90d234f9ecee605222415c39bafc881e2
nhuhoa <htran@bccrc.ca>
2022-07-22 00:29:31 +0000

    supp fig2

--- scripts/drug_manuscript/SUPP_figure2_SA609.R
+++ scripts/drug_manuscript/SUPP_figure2_SA609.R
@@ -186,7 +186,9 @@ plot_SUPP_fig2 <- function(){
   datatag <- 'SA609'
   
   medianCNV_plt_topplot <- readRDS(paste0(output_dir, datatag, '_DLP_heatmap.rds'))
-  p609_top_plt <- cowplot::plot_grid(medianCNV_plt_topplot$cnv_plot, NULL, rel_widths = c(3,1), nrow=1)
+  medianCNV_plt_topplot1 <- cowplot::plot_grid(medianCNV_plt_topplot$cnv_plot, medianCNV_plt_topplot$plg, rel_heights = c(1,0.1), ncol=1)
+  p609_top_plt <- cowplot::plot_grid(medianCNV_plt_topplot1, NULL, rel_widths = c(4,2), nrow=1)+ 
+    theme(plot.background = element_rect(fill = "white", colour = "white"))
   
   clone_plg_609 <- plot_clone_color_legend('SA609', base_dir, ncols_grid=2)
   
@@ -200,15 +202,18 @@ plot_SUPP_fig2 <- function(){
   p609_leftside <- cowplot::plot_grid(rna_SA609$UnRx_X4$p, rna_SA609$UnRx_X5$p, rna_SA609$UnRx_X6$p, rna_SA609$UnRx_X7$p,
                                    rna_SA609$Rx_X4$p,rna_SA609$Rx_X5$p, rna_SA609$Rx_X6$p,rna_SA609$Rx_X7$p,
                                    clone_plg_609, rna_SA609$RxH_X5$p,rna_SA609$RxH_X6$p, rna_SA609$RxH_X7$p,
-                                   ncol=4, align = 'hv')
+                                   ncol=4, align = 'hv')+
+    theme(plot.background = element_rect(fill = "white", colour = "white"))
   
-  p609_bottom <- cowplot::plot_grid(p609_leftside, res_prop10x_SA609$p, res_barDLP_609$p, rel_widths = c(4,1,1), nrow=1)
-  p609_total <- cowplot::plot_grid(p609_top_plt, p609_bottom,rel_heights = c(2,3), ncol=1)
+  p609_bottom <- cowplot::plot_grid(p609_leftside, res_prop10x_SA609$p, res_barDLP_609$p, rel_widths = c(4,1,1), nrow=1)+ 
+    theme(plot.background = element_rect(fill = "white", colour = "white"))
+  p609_total <- cowplot::plot_grid(p609_top_plt, p609_bottom,rel_heights = c(2,3), ncol=1) + 
+    theme(plot.background = element_rect(fill = "white", colour = "white"))
   
   ggsave(paste0(output_dir,"SUPPFig2_SA609.pdf"),
          plot = p609_total,
          height = 11,
-         width = 13,
+         width = 11,
          useDingbats=F, # from Sam's suggestion
          dpi = 150
   )
@@ -216,7 +221,7 @@ plot_SUPP_fig2 <- function(){
   ggsave(paste0(output_dir,"SUPPFig2_SA609.png"),
          plot = p609_total,
          height = 11,
-         width = 13,
+         width = 11,
          type = "cairo-png",
          dpi=150
   )
@@ -225,4 +230,149 @@ plot_SUPP_fig2 <- function(){
 
 
 
+
+
+
+
+
+# dat: data.frame with columns: (id, s)
+# clone_dic: a data.frame with columns: (id, K, letters, pretty_names, colour)
+# plot_box_posterior <- function(dat, clone_dic, show_title=TRUE, ref_clone_letter = '', clone_axis_dir = 'bottom', sort_by = 'median', add_linebreak = FALSE) {
+#   
+#   clone_dic$K <- as.character(clone_dic$K)
+#   dat <- dplyr::right_join(dat, clone_dic)
+#   dat <- dat[!is.na(dat$id), ]
+#   
+#   
+#   # Sort clones by their posterior mean fitness values
+#   if (sort_by == 'median') {
+#     s_means <- dat %>% dplyr::group_by(letters) %>% dplyr::summarise(mean_s = median(s)) %>% dplyr::arrange(mean_s)
+#   } else {
+#     s_means <- dat %>% dplyr::group_by(letters) %>% dplyr::summarise(mean_s = mean(s)) %>% dplyr::arrange(mean_s)
+#   }
+#   
+#   dat$letters <- factor(x = dat$letters, levels = s_means$letters, ordered = T)
+#   
+#   
+#   myColors <- clone_dic$colour
+#   names(myColors) <- clone_dic$letters
+#   
+#   dat$pretty_names <- factor(dat$pretty_names, levels = names(myColors), ordered = T)
+#   stopifnot(all(levels(dat$pretty_names) == as.character(names(myColors))))
+#   
+#   # change to s + 1
+#   dat$s <- dat$s + 1
+#   
+#   # Set the relevant s-values, length of the whiskers  
+#   # Put the min and max in range of the whiskers...
+#   whisker_coef <- 1.5
+#   
+#   lims <- dat %>% dplyr::group_by(K) %>% dplyr::summarise(iqr = IQR(s), median = median(s), q1 = quantile(s, .25), q3 = quantile(s, .75), bottom = min(s), top = max(s)) 
+#   
+#   p <- 
+#     dat %>% 
+#     ggplot(aes(letters, s, color = letters, fill = letters)) +
+#     geom_boxplot(outlier.shape = NA, coef = whisker_coef) + 
+#     stat_boxplot(geom = 'errorbar', width = 0.3, size = .5, coef = whisker_coef) + 
+#     scale_x_discrete(limits = levels(dat$letters), breaks = levels(dat$letters), labels = levels(dat$letters), position = clone_axis_dir) + 
+#     coord_flip(ylim = c(min(lims$bottom), max(lims$top))) + 
+#     geom_hline(yintercept = 1.0, linetype = 'longdash', size = .5) +
+#     scale_color_manual(values = myColors) + 
+#     scale_fill_manual(values = myColors) + 
+#     xlab('') + ylab(sprintf('1 + s (relative to clone %s)', ref_clone_letter)) +
+#     theme_light(base_size = 20) + 
+#     fitclone_get_theme_no_grid() + 
+#     theme(axis.ticks.y = element_blank())
+#   
+#   if (add_linebreak) {
+#     p <- p + ylab(sprintf('1 + s\n(rel. to clone %s)', ref_clone_letter))
+#   }
+#   
+#   
+#   if (show_title) {
+#     p <- p + ggtitle(paste0(datatag), subtitle = get_ID_from_edge_list_path(edge_list_path))
+#   } else {
+#     p <- p + theme(legend.position = "none")
+#   }
+#   
+#   data <- ggplot_build(p)$data[[1]]
+#   p <- p + geom_segment(inherit.aes = FALSE, data=data, aes(x=xmin, xend=xmax, y=middle, yend=middle), color="white", size=.5)
+#   
+#   return(p)
+# }
+
+
+# plot_box_posterior_v2 <- function(dat, clone_dic, show_title=TRUE, ref_clone_letter = '', 
+#                                   clone_axis_dir = 'bottom', sort_by = 'median', add_linebreak = FALSE) {
+#   
+#   letters=clone_name
+#   s=median_1_plus_s
+#   pretty_names
+#   coeff_df$clone_name
+#   
+#   # Sort clones by their posterior mean fitness values
+#   if (sort_by == 'median') {
+#     s_means <- dat %>% dplyr::group_by(letters) %>% dplyr::summarise(mean_s = median(s)) %>% dplyr::arrange(mean_s)
+#   } else {
+#     s_means <- dat %>% dplyr::group_by(letters) %>% dplyr::summarise(mean_s = mean(s)) %>% dplyr::arrange(mean_s)
+#   }
+#   
+#   dat$letters <- factor(x = dat$letters, levels = s_means$letters, ordered = T)
+#   
+#   
+#   myColors <- clone_dic$colour
+#   names(myColors) <- clone_dic$letters
+#   
+#   dat$pretty_names <- factor(dat$pretty_names, levels = names(myColors), ordered = T)
+#   stopifnot(all(levels(dat$pretty_names) == as.character(names(myColors))))
+#   
+#   # change to s + 1
+#   # dat$s <- dat$s + 1
+#   
+#   # Set the relevant s-values, length of the whiskers  
+#   # Put the min and max in range of the whiskers...
+#   whisker_coef <- 1.5
+#   
+#   lims <- dat %>% dplyr::group_by(K) %>% dplyr::summarise(iqr = IQR(s), median = median(s), q1 = quantile(s, .25), q3 = quantile(s, .75), bottom = min(s), top = max(s)) 
+#   
+#   p <- 
+#     dat %>% 
+#     ggplot(aes(letters, s, color = letters, fill = letters)) +
+#     geom_boxplot(outlier.shape = NA, coef = whisker_coef) + 
+#     stat_boxplot(geom = 'errorbar', width = 0.3, size = .5, coef = whisker_coef) + 
+#     scale_x_discrete(limits = levels(dat$letters), breaks = levels(dat$letters), labels = levels(dat$letters), position = clone_axis_dir) + 
+#     coord_flip(ylim = c(min(lims$bottom), max(lims$top))) + 
+#     geom_hline(yintercept = 1.0, linetype = 'longdash', size = .5) +
+#     scale_color_manual(values = myColors) + 
+#     scale_fill_manual(values = myColors) + 
+#     xlab('') + ylab(sprintf('1 + s (relative to clone %s)', ref_clone_letter)) +
+#     theme_light(base_size = 20) + 
+#     fitclone_get_theme_no_grid() + 
+#     theme(axis.ticks.y = element_blank())
+#   
+#   if (add_linebreak) {
+#     p <- p + ylab(sprintf('1 + s\n(rel. to clone %s)', ref_clone_letter))
+#   }
+#   
+#   
+#   p <- p + theme(legend.position = "none")
+#   
+#   data <- ggplot_build(p)$data[[1]]
+#   p <- p + geom_segment(inherit.aes = FALSE, data=data, aes(x=xmin, xend=xmax, y=middle, yend=middle), color="white", size=.5)
+#   
+#   return(p)
+# }
+
+plot_coefficients <- function(){
+  base_dir <- '/home/htran/Projects/farhia_project/drug_resistant_material/'
+  input_dir <- paste0(base_dir, 'materials/fitness_paper_DLP/')
+  output_dir <- paste0(base_dir, 'materials/umap_figs/figs_rna/')
+  coeff_df <- data.table::fread(paste0(input_dir, 'SUPP_Table2_fitness_coefficients.csv')) %>% as.data.frame()
+  
+  
+  coeff_df <- coeff_df %>%
+    dplyr::filter(datasetname=='SA609')
+  dim(coeff_df)
+  
+}
 
