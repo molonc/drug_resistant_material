4d647e2f609e28b8063f42f5354236d6b6391735
nhuhoa <htran@bccrc.ca>
2023-08-03 20:25:51 +0000

    Updating entire repo

--- materials/compress_csv.sh
+++ materials/compress_csv.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-search_dir=/home/htran/Projects/farhia_project/drug_resistant_material/materials/fitness_paper_DLP/
+search_dir=/home/htran/Projects/farhia_project/drug_resistant_material/materials/Supplementary_Tables/
 
 input_csvs=$(find $search_dir -type f -name "*.csv")
 
